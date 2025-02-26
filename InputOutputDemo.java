9690f98ddd51811f770f1ea7e57a8a68cb50ad26
Nassri Lakiman <lakimannassri@bscs@plmun.edu.ph>
2025-02-26 23:48:04 +0800

    nass3

--- InputOutputDemo.java
+++ InputOutputDemo.java
@@ -1,22 +1,21 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class InputOutputDemo {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
-        Scanner input = new Scanner (System.in);
-
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter an integer: ");
         int userInt = input.nextInt();
 
-        System.out.print ("Enter a decimal number: ");
-        double userDouble = input.nextDouble ();
-        input.nextLine();
+        System.out.print("Enter a decimal number: ");
+        double userDouble = input.nextDouble();
+        input.nextLine(); // Consume newline
 
-        System.out.print ("Enter a word or sentence: ");
+        System.out.print("Enter a word or sentence: ");
         String userString = input.nextLine();
-        
-        System.out.printf("Your integer:  %d%n", userInt);
-        System.out.printf("Your decimal:  %.2f%n", userDouble);
+
+        System.out.printf("Your integer: %d%n", userInt);
+        System.out.printf("Your decimal number: %.2f%n", userDouble);
         System.out.printf("Your string: %s%n", userString);
         input.close();
 
